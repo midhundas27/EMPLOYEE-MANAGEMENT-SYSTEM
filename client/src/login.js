929d6452edf34fa2b7a8d5b9efaeb2ea8a75d9d1
amalut <amalut9@gmail.com>
2023-10-29 11:19:56 +0530

    login updated

--- client/src/login.js
+++ client/src/login.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
+import {useNavigate } from "react-router-dom";
 import './login.css'
 const Login = () => {
-        const [loginAs, setLoginAs] = useState("admin")
-
-        const handleLoginAsChange = (event) => {
-          setLoginAs(event.target.value)}
+        //const [loginAs, setLoginAs] = useState("admin")
+        const [username,setUsername] = useState(null)
+        const [password,setPassword] = useState(null)
+        const navigate = useNavigate();
 
         const handleLoginFormSubmit = (event) => {     // Handle sign-in logic
           event.preventDefault()
+          if(username==="rashin" && password==="12345"){
+            navigate("/home")
+          }
           console.log('Sign In Form Submitted')
-          console.log('Sign In As:', loginAs)}
+          // console.log('Sign In As:', loginAs)
+        }
 
   return (
     <div className='login-background'>
@@ -18,8 +23,8 @@ const Login = () => {
       <p className="login-text">Sign In</p><br></br>
       <form onSubmit={handleLoginFormSubmit} class="login-form">
         <input type="text" required placeholder="Username"
-          id='username' onChange={handleLoginAsChange} className="login-username"/><br></br>
-        <input type="text" required placeholder="Password" id='password' onChange={handleLoginAsChange}
+          id='username' onChange={(e)=>setUsername(e.target.value)} className="login-username"/><br></br>
+        <input type="text" required placeholder="Password" id='password' onChange={(e)=>setPassword(e.target.value)}
           className="login-password"/><br></br>
         <input type="submit" value="Sign In" className="login-button"/>
       </form>
