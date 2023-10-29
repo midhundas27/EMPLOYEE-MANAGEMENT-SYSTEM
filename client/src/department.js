56529c561dd2e5fcfd44ae6f5dae3dd5e1081acc
amalut <amalut9@gmail.com>
2023-10-29 13:42:22 +0530

    department cards updated

--- client/src/department.js
+++ client/src/department.js
@@ -8,10 +8,10 @@ function Department() {
       <TopNav4dept/>
       <h1 class="dept-text">Departments</h1>
       <div className="depts">
-      <Card4dept/>
-      <Card4dept/>
-      <Card4dept/>
-      <Card4dept/>
+      <Card4dept deptname="Finance and Accounting" email="finace@gmail.com" location="Kozhikode" logo="" onClick=""/>
+      <Card4dept deptname="Marketing and HR" email="finace@gmail.com" location="Kozhikode" logo="" onClick=""/>
+      <Card4dept deptname="Worker" email="finace@gmail.com" location="Kozhikode" logo="" onClick=""/>
+      <Card4dept deptname="Slaes" email="finace@gmail.com" location="Kozhikode" logo="" onClick=""/>
       </div>
     </div>
   );
