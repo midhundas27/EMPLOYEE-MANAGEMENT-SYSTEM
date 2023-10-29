3b4dcce15a447e529573d46a902427dd26b45ff2
Midhun Das <dasmidhun27@gmail.com>
2023-10-29 11:21:41 +0530

    dept page modified

---
+++ client/src/components/MainSection/components/card4dept.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card4dept = () => {
+
+  return (
+    <div className="card4dept-component">
+      <div className="card4dept-inner">
+        <div className="dropdownContainer">
+        </div>
+        {/* <div className="profileImage">
+          <img
+          src="/image"
+            alt="firstname"
+          /> 
+        </div>*/}
+        <div className="dept-detail">
+          <h3>deptname</h3>
+          <p>email</p>
+        </div>
+      </div>
+      <div className="job-dept">
+        <p>job</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card4dept;
--- client/src/department.js
+++ client/src/department.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './department.css'
 import TopNav4dept from './components/TopNav/TopNav4dept';
+import Card4dept from './components/MainSection/components/card4dept';
 function Department() {
   return (
     <div className="dept-container">
       <TopNav4dept/>
       <h1 class="dept-text">Departments</h1>
+      <div className="depts">
+      <Card4dept/>
+      <Card4dept/>
+      <Card4dept/>
+      <Card4dept/>
+      </div>
     </div>
   );
 }
